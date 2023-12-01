748ea8f44aec0c2bfa20fdba7c2122dee7b49cfd
Ranjeet Kumar Raj <121329638+RanjeetRaj444@users.noreply.github.com>
2023-12-01 08:38:48 +0530

    Update project.routes.js

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const projectModel = require("../models/project.model");
+const upload = require("../middleware/upload");
 const router = express.Router();
 router.get("/project/:id", async (req, res) => {
 	// /:id-- userId 
@@ -24,6 +25,25 @@ router.post("/add_project", async (req, res) => {
 				.send({ project: exist, msg: "Project is already exist!😒" });
 		const data = await projectModel.create(project);
 		res.status(200).send({ data: data, msg: "Project stored in database😊" });
+	} catch (err) {
+		res.status(500).send({ error: err.message });
+	}
+});
+const cpUpload = upload.fields([{ name: "image", maxCount: 1 }]);
+router.patch("/update_project/:id", cpUpload, async (req, res) => {
+	//  /:id  --- current user id
+	const updateProject = {
+		image: "",
+	};
+	if (req.files.image) updateProject.image = req.files.image[0].path;
+	// console.log(updateUser);
+	try {
+		const data = await projectModel.findByIdAndUpdate(
+			{ _id: req.params.id },
+			updateProject,
+			{ new: true },
+		);
+		res.status(200).send({ data: data });
 	} catch (err) {
 		res.status(500).send({ error: err.message });
 	}
